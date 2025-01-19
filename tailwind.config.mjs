11d2a51eee33f75304ac50a07ca021888a6495e0
voydd <deandrerandolph441@gmail.com>
2025-01-19 10:50:11 -0500

    Initial Push

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,44 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#b91c1c',
+
+          secondary: '#0000ff',
+
+          accent: '#a3e635',
+
+          neutral: '#2b2523',
+
+          'base-100': '#1f2937',
+
+          info: '#00d5ff',
+
+          success: '#008900',
+
+          warning: '#f7b000',
+
+          error: '#ff7885',
+        },
+      },
+    ],
+  },
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
-  plugins: [],
-};
+  plugins: [require('daisyui')],
+}
+
+export default config
